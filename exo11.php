0970afaacc87d553df82189a58adb6aceeb0ca79
ktroisn <brouillonquentin@gmail.com>
2023-03-27 15:05:08 +0200

    exo11

--- exo11.php
+++ exo11.php
@@ -8,5 +8,20 @@ formaterDateFr("2018-02-23");
 <h2>Résultat</h2>
 
 <?php
-
+    $date = "2018-02-23";
+    echo formaterDateFr($date);
+    // Définition de la fonction
+    function formaterDateFr($date){ 
+        // Utilisation intldateformatter  
+        $dateFr = new DateTime($date);  
+        $fonctionDateFormat = new IntlDateFormatter(
+            "fr_FR",
+            IntlDateFormatter::FULL,//ajoute le jour de la semaine
+            IntlDateFormatter::NONE,//supprime les heure après
+            'Europe/Paris', 
+            IntlDateFormatter::GREGORIAN  );  
+        //Mise en forme de la date 
+        $fonctionDateFormat->setPattern('EEEE dd MMMM YYYY');  //ce pattern crée le format (jours date mois année)
+        return $fonctionDateFormat->format($dateFr);
+    };
 ?>
